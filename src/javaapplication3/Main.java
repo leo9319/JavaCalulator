1186647f20fb7401a25fa44cf6d52b921d13c1c8
leo9319 <leo_9319@yahoo.com>
2016-08-10 21:49:41 +0600

    first commit

---
+++ src/javaapplication3/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication3;
+
+/**
+ *
+ * @author Leo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Calculator calculator = new Calculator();
+        calculator.setVisible(true);
+    }
+
+}
